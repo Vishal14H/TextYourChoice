60b89bef921b026058477725bb0df69dd01d48f0
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2020-08-30 20:13:58 +0530

    1st decision Scenario

---
+++ Text Your Choice 1/Assets/State.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+  [TextArea(20,10)] [SerializeField] string storyText;
+}
